e1a9de78949dfe25672ba74f90c95ddb98f85dcd
timittt <timpantoffelmann@gmail.com>
2026-02-25 15:02:23 +0100

    add struct Stats

--- include/stats.h
+++ include/stats.h
@@ -4,16 +4,28 @@
 #include <stdio.h>
 
 /** ### HEADER FILE FOR CALCULATING STATS ###
-* 
-*/
+ * 
+ */
 
 /**
-* @brief Write first two lines of text file
-*
-* @param[in] car_count number of occupied spaces
-* @param[in] spaces_count total number of parking spaces
-* @return occupancy rate percentage
-*/
+ * @brief Stores the stats that are logged every timestep
+ *
+ */
+typedef struct {
+    float occupancy;
+    int cars_waiting;
+    float avg_wait_time;
+    int first_car_wait_time;
+    float avg_remaining_parking_time;
+} Stats;
+
+/**
+ * @brief Write first two lines of text file
+ *
+ * @param[in] car_count number of occupied spaces
+ * @param[in] spaces_count total number of parking spaces
+ * @return occupancy rate percentage
+ */
 float get_occupancy_rate(int car_count, int spaces_count);
 
 #endif
