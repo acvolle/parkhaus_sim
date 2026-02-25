f3827dd588c79760569b4e2b2a899c3f224caee6
timittt <timpantoffelmann@gmail.com>
2026-02-25 15:55:34 +0100

    add functions for specific stats

--- include/stats.h
+++ include/stats.h
@@ -12,7 +12,7 @@
  *
  */
 typedef struct {
-    float occupancy;
+    float occupancy_rate;
     int cars_waiting;
     int first_car_wait_time;
     float avg_wait_time;
@@ -20,12 +20,41 @@ typedef struct {
 } Stats;
 
 /**
- * @brief Write first two lines of text file
+ * @brief Write the current occupancy rate into a struct Stats
  *
  * @param[in] car_count number of occupied spaces
  * @param[in] spaces_count total number of parking spaces
- * @return occupancy rate percentage
+ * @param[out] p_stats reference to struct Stats
+ * @return 0 for sucess, -1 for error
  */
-float get_occupancy_rate(int car_count, int spaces_count);
+int stats_occupancy_rate(int car_count, int spaces_count, Stats *p_stats);
+
+/**
+ * @brief Write the current number of waiting cars into a struct Stats
+ *
+ * @param[in] queued_cars number of waiting cars
+ * @param[out] p_stats reference to struct Stats
+ * @return 0 for sucess, -1 for error
+ */
+int stats_cars_waiting(int queued_cars, Stats *p_stats);
+
+/**
+ * @brief Write the current waiting time of the first car into a struct Stats
+ *
+ * @param[in] waiting_time waiting time of the next car to leave the queue
+ * @param[out] p_stats reference to struct Stats
+ * @return 0 for sucess, -1 for error
+ */
+int stats_first_car_waiting_time(int waiting_time, Stats *p_stats);
+
+/**
+ * @brief Write the current number of waiting cars into a struct Stats
+ *
+ * @param[in] 
+ * @param[out] p_stats reference to struct Stats
+ * @return 0 for sucess, -1 for error
+ */
+int stats_avg_wait_time(int , Stats *p_stats);
+
 
 #endif
