15b691b4e53d7ac93c49316c4ef1f731b631246d
andrew <andrewcvolle@gmail.com>
2026-02-25 15:55:54 +0100

    declare and prototype run_timestep()

--- include/simulation.h
+++ include/simulation.h
@@ -74,7 +74,23 @@ static int car_gen_bool(const int probability);
 */
 static int generate_car(Parkhaus *p_parkhaus, Queue *p_queue);
 
-int 
+/**
+* @brief Runs a single timestep of the simualtion
+*
+* Updates the `Parkhaus` struct.
+* If there are `Car` structs in the Queue, as many as have space in the `Parkhaus` struct are parked in the
+* `Parkhaus` struct (via their pointers).
+* Randomly (using simulation parameters) determines whether a new `Car` struct is generated, and
+* then parks it (if there is a free space) or else enqueues it in the `Queue`. 
+*
+* @param[out] p_parkhaus Pointer to the Parkhaus struct where a Car might be parked.
+* @param[out] p_queue Pointer to the Queue which the Car might be added to.
+* @param[in] p_config Pointer to the Config struct of the current simulation.
+* @return * @return int Status code:
+*            - `0` if the operation succeeded.
+*            - `-1` if an error occurred (e.g., pointer is `NULL`).
+*/
+int run_timestep(Parkhaus *p_parkhaus, Queue *p_queue, Config *p_config);
 
 #endif
 
