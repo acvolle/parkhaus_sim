b0c066c45bc6e3217b655e297d33742b0d90a0c2
andrew <andrewcvolle@gmail.com>
2026-02-27 15:04:28 +0100

    add public functions to simualtion.c

---
+++ src/simulation.c
@@ -0,0 +1,22 @@
+#include "../include/simulation.h"
+
+// FOR DOCUMENTATION OF NON-STATIC FUNCTIONS, SEE SIMULATION.H
+
+//Initializes a new Config struct
+Config *new_config(){
+
+}
+
+
+// Frees the memory allocated to the config struct
+int free_config(Config *p_config){
+
+}
+
+
+// Runs a single timestep of the simulation
+int run_timestep(Parkhaus *p_parkhaus, Queue *p_queue, Config *p_config, Stats *p_stats){
+
+}
+
+
