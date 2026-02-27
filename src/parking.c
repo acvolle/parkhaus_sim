8b084c77b7cb5367fbbaf1b188cba7f4640058b4
andrew <andrewcvolle@gmail.com>
2026-02-27 15:53:08 +0100

    set up parking.c file

---
+++ src/parking.c
@@ -0,0 +1,35 @@
+#include "../include/parking.h"
+
+// Initialize new Parkhaus
+
+Parkhaus *init_parkhaus(unsigned int spaces_count){
+
+}
+
+
+
+//Delete Parkhaus and free allocated memory
+int close_parkhaus(Parkhaus *p_parkhaus){
+
+}
+
+
+
+// Inserts new Car into the spaces array of a Parkhaus
+int park_car(Parkhaus *p_parkhaus, Car *p_car){
+
+}
+
+
+
+//Updates all Car structs in a Parkhaus struct
+int update_parkhaus(Parkhaus *p_parkhaus){
+
+}
+
+
+
+//Checks if a Parkhaus is full
+int parkhaus_is_full(Parkhaus *p_parkhaus){
+    
+}
