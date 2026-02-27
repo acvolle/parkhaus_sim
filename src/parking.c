22c8c4ac1f0ed662919579ea957c125624be6f0b
andrew <andrewcvolle@gmail.com>
2026-02-27 15:54:15 +0100

    write pseudocode for update_parkhaus() function

--- src/parking.c
+++ src/parking.c
@@ -24,12 +24,27 @@ int park_car(Parkhaus *p_parkhaus, Car *p_car){
 
 //Updates all Car structs in a Parkhaus struct
 int update_parkhaus(Parkhaus *p_parkhaus){
-
+/*
+    IF p_parkhaus != NULL THEN
+        FOR i<-0 TO p_parkhaus->size DO
+            IF Car pointer in array[i] != NULL THEN
+                Car pointer->remaining time --;
+                IF remaining time == 0 THEN
+                    free memory of Car
+                    delete pointer to Car from array
+                END IF
+            END IF
+        END FOR
+        return 0
+    ELSE
+        return -1
+    END IF
+*/
 }
 
 
 
 //Checks if a Parkhaus is full
 int parkhaus_is_full(Parkhaus *p_parkhaus){
-    
+
 }
