7d0f61a9d8152ef13306c43cb211a85233b783f6
timittt <timpantoffelmann@gmail.com>
2026-02-28 22:09:47 +0100

    format ui.c file

---
+++ src/ui.c
@@ -0,0 +1,71 @@
+/**
+ * THIS FILE INCLUDES THE LOGIC FOR ALL UI FUNCTIONS
+ */
+
+#include <stdio.h>
+#include "ui.h"
+#include "simulation.h"
+
+/* PSEUDOCODE
+
+*/
+void ui_print_border(void)
+{
+
+}
+
+/* PSEUDOCODE
+
+*/
+void ui_print_welcome(void)
+{
+
+}
+
+/* PSEUDOCODE
+
+*/
+int ui_get_int(const char *prompt, int min, int max)
+{
+
+}
+
+/* PSEUDOCODE
+
+*/
+int ui_get_params(Config *p_config)
+{
+
+}
+
+/* PSEUDOCODE
+
+*/
+int ui_write_head(const Config *p_config, const char *stats_names, FILE *fp)
+{
+
+}
+
+/* PSEUDOCODE
+
+*/
+int ui_write_stats(const Stats *p_stats, FILE *fp)
+{
+
+}
+
+/* PSEUDOCODE
+
+*/
+void ui_print_stats(const Stats *p_stats)
+{
+
+}
+
+/* PSEUDOCODE
+
+*/
+void ui_print_final_stats(int avg_parking_duration, int avg_waiting_duration, int avg_stress_score)
+{
+
+}
