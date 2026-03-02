f44b2dd639e286acf48626ea91a5bb016f406715
timittt <timpantoffelmann@gmail.com>
2026-03-02 16:56:11 +0100

    add pseudocode

--- src/queue.c
+++ src/queue.c
@@ -12,7 +12,7 @@
 
 void queue_init(Queue *p_queue)
 /* PSEUDOCODE
-IF p_queue != NULL
+IF p_queue != NULL THEN
     p_queue <- allocate memory the size of a struct Queue
 ELSE
     OUTPUT error message
@@ -24,6 +24,11 @@ END IF
 
 bool queue_is_empty(const Queue *p_queue)
 /* PSEUDOCODE
+IF p_queue->count = 0 THEN
+    return true
+ELSE
+    return false
+END IF
 */
 {
 
@@ -31,7 +36,27 @@ bool queue_is_empty(const Queue *p_queue)
 
 int enqueue(Queue *p_queue, Car *p_car)
 /* PSEUDOCODE
-IF queue_is_sm
+IF p_queue = NULL OR p_car = NULL THEN
+    return -1
+END IF
+
+new_node <- allocate memory the size of a Node
+IF new_node = NULL THEN 
+    return -1 (Memory Error)
+END IF
+
+new_node->car <- *p_car
+new_node->p_next <- NULL
+
+IF queue_is_empty THEN
+    p_queue->p_head <- new_node
+ELSE
+    p_queue->p_tail->p_next <- new_node
+END IF
+
+p_queue->p_tail <- new_node                 // new_node is now the tail of the queue
+p_queue->count <- p_queue->count + 1        // count up
+return 0
 */
 {
 
@@ -39,6 +64,9 @@ IF queue_is_sm
 
 int dequeue(Queue *p_queue, Car *p_dequeued_car)
 /* PSEUDOCODE
+IF p_queue = NULL OR p_car = NULL THEN
+    return -1
+END IF
 */
 {
 
@@ -46,6 +74,15 @@ int dequeue(Queue *p_queue, Car *p_dequeued_car)
 
 int queue_increase_wait_time(Queue *p_queue)
 /* PSEUDOCODE
+IF p_queue is NULL THEN
+    return -1
+END IF
+Node* current <- p_queue->p_head
+WHILE current != NULL DO
+    current->car.time_in_queue++
+    current <- current->p_next
+END WHILE
+return 0
 */
 {
 
