a0c901c25c51583282becc21993618e20de30f37
Gagg <elias.gagg@gmail.com>
2026-03-02 23:30:13 +0100

    add return pointer to init_car, add missing parts of delete car

--- src/car.c
+++ src/car.c
@@ -8,6 +8,7 @@ Car* init_car(int  id_number, int current_time, int max_duration)
      * IF car pointer == NULL 
      *  print "ERRROR, memory allocation for car has failed"
      *  return NULL
+     *  return car pointer
      * ENDIF
      */
 }
@@ -16,8 +17,9 @@ int delete_car(Car *p_Cardelete )
 /**
  * IF p_Cardelete pointer != NULL
  *  free(p_Cardelete)
- *
- *  
+ *  return 0 in case of success
+ *  return -1 in case of failure
+ * ENDIF
  * 
  */
 };
