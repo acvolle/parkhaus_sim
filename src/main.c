98e4f7fbf50705fec9d580312b2957b54fb88c2a
andrew <andrewcvolle@gmail.com>
2026-03-03 14:33:25 +0100

    add specific function names to main pseudocode

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ OUTPUT welcome message (ui_get_welcome())
 Config pointer <- new_config()
 Get Simulation parameters (ui_get_params)
 Write header in text file (ui_write_head)
+Stats pointer <- stats_create()
 Queue pointer <- queue_init()
 Parkhaus pointer <- init_parkhaus(Config pointer->num_spaces)
 
@@ -19,12 +20,15 @@ FOR i <- 0 TO Config pointer->simulation_duration-1 DO
         OUTPUT error message
         return 1
     END IF
+    current_timestep++
 END FOR
 
 
 //////SHUTDOWN//////
 ui_print_final_stats()
 Close text file 
+Delete Config stuct (free_config)
+Delete Stats struct (stats_delete)
 Clear Queue (clear_queue)
 Close Parkhaus (close_parkhaus)
 return 0
