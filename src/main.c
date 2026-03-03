ce1fc38040458935ceda6b8e1c35067ba282bfd3
andrew <andrewcvolle@gmail.com>
2026-03-03 21:02:44 +0100

    make minor fixes and additions to main pseudocode

--- src/main.c
+++ src/main.c
@@ -9,21 +9,24 @@ OUTPUT welcome message (ui_get_welcome())
 Config pointer <- new_config()
 IF Get Simulation parameters (ui_get_params) == -1 THEN
     free_config(Config pointer)
+    close text file
     return 1
 END IF
 IF Write header in text file (ui_write_head) == -1 THEN
-free_config(Config pointer)
+    free_config(Config pointer)
+    close text file
     return 1
 END IF
 Stats pointer <- stats_create()
 New Queue
 queue_init(pointer to Queue)
 Parkhaus pointer <- init_parkhaus(Config pointer->num_spaces)
+srand(p_config->random_seed)
 
 
 //////LOOP//////
 FOR i <- 0 TO Config pointer->simulation_duration-1 DO
-    IF run_timestep == -1 THEN  //function from simulation.c that runs all operational steps
+    IF run_timestep(Parkhaus pointer, Queue pointer, Config pointer, Stats pointer) == -1 THEN  //function from simulation.c that runs all operational steps
         OUTPUT error message
         break for loop //shuts down the simulation, doesn't return as allocated memory must still be freed
     END IF
