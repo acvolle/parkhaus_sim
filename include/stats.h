1b708502be215041b86332ad82aa797faae725e5
timittt <timpantoffelmann@gmail.com>
2026-03-03 11:27:12 +0100

    add explanation to header

--- include/stats.h
+++ include/stats.h
@@ -1,12 +1,22 @@
+/** ### HEADER FILE FOR CALCULATING STATS ###
+ * This header file defines a structure "Stats"
+ * which stores the int/float statistics
+ * that are updated everey timestep.
+ * The memory for this struct is allocated dynamically,
+ * therefore the functions ´stats_create´ and ´stats_delete´ are needed.
+ * Additionally, stats_clear can be used to set all the stored stats 0.
+ * The "stress score" is a score 0-100, which represents the overall
+ * stress of the current parking situation.
+ * To update the statistics for each timestep correctly,
+ * the functions ´stats_occupancy_rate´ and ´stats_queue_stats´ have to
+ * be called first before ´stats_stress_score´, so that the stress score
+ * is calculated with the updated latest values.
+ */
 #ifndef STATS_H
 #define STATS_H
 
 #include <stdio.h>
 #include "queue.h"
-
-/** ### HEADER FILE FOR CALCULATING STATS ###
- * 
- */
 
 /**
  * @brief Stores the stats that are logged every timestep
@@ -46,6 +56,9 @@ int stats_delete(Stats *p_stats);
 /**
  * @brief Write the current occupancy rate into a struct Stats
  *
+ * The function calculates the ratio of parked cars / available spaces
+ * and stores it into stats.
+ * 
  * @param[in] car_count number of occupied spaces
  * @param[in] spaces_count total number of parking spaces
  * @param[out] p_stats reference to struct Stats
@@ -71,7 +84,7 @@ int stats_queue_stats(const Queue *p_queue, Stats *p_stats);
  * @brief Update the stress score
  *
  * This function reads the gathered stats,
- * calculates the stess score
+ * calculates the stress score
  * (exact formula tbd!!!!!!!!!!)
  * and writes the new score into the struct Stats
  * 
