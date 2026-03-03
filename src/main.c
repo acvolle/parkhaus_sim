8068ac8fd3f5d4e60dade676a7037bdc4b983362
andrew <andrewcvolle@gmail.com>
2026-03-03 14:40:04 +0100

    add error handling to main pseudocode

--- src/main.c
+++ src/main.c
@@ -7,8 +7,12 @@ int main(){
 Open new text file
 OUTPUT welcome message (ui_get_welcome())
 Config pointer <- new_config()
-Get Simulation parameters (ui_get_params)
-Write header in text file (ui_write_head)
+IF Get Simulation parameters (ui_get_params) == -1 THEN
+    return 1
+END IF
+IF Write header in text file (ui_write_head) == -1 THEN
+    return 1
+END IF
 Stats pointer <- stats_create()
 Queue pointer <- queue_init()
 Parkhaus pointer <- init_parkhaus(Config pointer->num_spaces)
@@ -26,11 +30,21 @@ END FOR
 
 //////SHUTDOWN//////
 ui_print_final_stats()
-Close text file 
-Delete Config stuct (free_config)
-Delete Stats struct (stats_delete)
+IF Close text file == -1 THEN
+    return 1
+END IF
+IF Delete Config stuct (free_config) == -1 THEN
+    return 1
+END IF
+IF Delete Stats struct (stats_delete) == -1 THEN
+    return 1
+END IF
 Clear Queue (clear_queue)
-Close Parkhaus (close_parkhaus)
+IF Close Parkhaus (close_parkhaus) == -1 THEN
+    return 1
+END IF
+
+
 return 0
 
 
