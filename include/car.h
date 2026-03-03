37ab73afe31511bf2eed1b21e7872963c068deff
timittt <timpantoffelmann@gmail.com>
2026-03-03 09:53:14 +0100

    improve car.h commentary

--- include/car.h
+++ include/car.h
@@ -2,7 +2,7 @@
  * - all functions include the word car
  * - defines struct car for use
  * - init_car creates a car based on the parameters from the Simulation
- * - remove_car deletes car after parking span ends
+ * - delete_car frees memory allocated to a Car struct
  */
 
 #ifndef CAR_H
@@ -41,8 +41,8 @@ Car* init_car(int  id_number, int current_time, int max_duration);
  * @brief deletes a struct of the type car
  * 
  * removes the dynamically created car as it no longer serves
- * any porpuse after it exits the garage (Parkhaus)
- * expect return 0 when succesful, return -1 when functions has failed
+ * any purpose after it exits the garage (Parkhaus)
+ * expect return 0 when succesful, return -1 when function has failed
  * 
  * @param[in] p_Cardelete 
  * @return int 
