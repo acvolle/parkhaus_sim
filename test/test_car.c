feba43f570e6441d157747abbc9949d8b060020d
andrew <andrewcvolle@gmail.com>
2026-03-11 17:03:09 +0100

    implement test_init_car function

--- test/test_car.c
+++ test/test_car.c
@@ -5,3 +5,20 @@
 int main(){
     
 }
+
+void test_init_car(){
+    
+    Car *p_car = init_car(1, 1, 1);
+
+    //asserts that init_car returns a pointer to something the size of a Car (presumably a Car struct)
+    assert(sizeof (struct Car) == (sizeof *p_car));
+
+    // asserts that assigning the ID number works
+    assert(p_car->id_number == 1);
+
+    //
+    assert(p_car->time_in_queue == 0);
+
+    free(p_car);
+
+}
