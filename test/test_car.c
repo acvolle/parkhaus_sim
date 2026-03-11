adf1bb80dc2358f9e226c9c807f1d181cd5012a5
andrew <andrewcvolle@gmail.com>
2026-03-11 21:20:45 +0100

    add checks for null pointers

--- test/test_car.c
+++ test/test_car.c
@@ -3,6 +3,12 @@
 #include "car.h"
 
 int main(){
+    //unit test init_car function
+    test_init_car();
+    //unit test delete_car function
+    test_delete_car();
+
+    return 0;
     
 }
 
@@ -10,8 +16,12 @@ void test_init_car(){
     
     Car *p_car = init_car(1, 1, 1);
 
+    if(p_car == NULL){
+        return;
+    }
+
     //asserts that init_car returns a pointer to something the size of a Car (presumably a Car struct)
-    assert(sizeof (struct Car) == (sizeof *p_car));
+    assert(sizeof(Car) == (sizeof *p_car));
 
     // asserts that assigning the ID number works
     assert(p_car->id_number == 1);
@@ -25,6 +35,10 @@ void test_init_car(){
 
 void test_delete_car(){
     Car *p_car = init_car(2, 3, 5);
+
+    if(p_car == NULL){
+        return;
+    }
     
     //asserts that delete_car successfully frees the memory of a Car pointer
     assert(delete_car(p_car) == 0);
@@ -33,7 +47,5 @@ void test_delete_car(){
 
     //asserts that passing a null pointer returns a managable error message
     assert(delete_car(p_car) == -1);
-
-
 
 }
