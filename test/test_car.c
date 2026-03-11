705f7f1f971f8fbabf66a748e22df3796eda2ce6
andrew <andrewcvolle@gmail.com>
2026-03-11 18:48:48 +0100

    implement test_delete_car function

--- test/test_car.c
+++ test/test_car.c
@@ -16,9 +16,24 @@ void test_init_car(){
     // asserts that assigning the ID number works
     assert(p_car->id_number == 1);
 
-    //
+    //asserts that the time in queue is zero-initialized
     assert(p_car->time_in_queue == 0);
 
     free(p_car);
+
+}
+
+void test_delete_car(){
+    Car *p_car = init_car(2, 3, 5);
+    
+    //asserts that delete_car successfully frees the memory of a Car pointer
+    assert(delete_car(p_car) == 0);
+
+    p_car = NULL;
+
+    //asserts that passing a null pointer returns a managable error message
+    assert(delete_car(p_car) == -1);
+
+
 
 }
