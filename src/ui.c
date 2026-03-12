e59e3aa8e1cd8113ecb1744ec3789b282aaf1794
andrew <andrewcvolle@gmail.com>
2026-03-12 08:46:23 +0100

    add extra error handling

--- src/ui.c
+++ src/ui.c
@@ -1,6 +1,6 @@
 /**
  * THIS FILE INCLUDES THE LOGIC FOR ALL UI FUNCTIONS
- * 
+ *
  * This file includes two static help functions ´ui_print_border´ and ´ui_get_int´
  * as well as the functions from ui.h that are called by the ´main.c´.
  */
@@ -19,21 +19,18 @@ static void ui_print_border(void)
 OUTPUT visual border
 */
 {
-
 }
-
 
 void ui_print_welcome(void)
 /*  PSEUDOCODE
 OUTPUT welcome message
 */
 {
-
 }
 
 /**
  * @brief Get an integer value from user input.
- * 
+ *
  * Asks the user to input an integer,
  * then checks if input is valid and integer is between min and max value.
  * If not, ask the user again and specify the min and max values.
@@ -57,32 +54,40 @@ END WHILE
 */
 {
     int value;
-    char input[10];
-    char *end_of_value; 
-    while(1){
+    char input[100];
+    char *end_of_value = input;
+    while (1)
+    {
         printf("%s", prompt);
-        if(fgets(input, sizeof input, stdin) == NULL){
+        if (fgets(input, sizeof input, stdin) == NULL)
+        {
             printf("Error reading input: Try again!\n");
-            
         }
 
         long input_val = strtol(input, end_of_value, 10);
 
-        if(end_of_value == input || (*end_of_value != '\n')){
+        if (end_of_value == input || (*end_of_value != '\n'))
+        {
+            clearerr(stdin);
             printf("Not an integer! Try again!\n");
             continue;
         }
 
-        value = (int)input_val; //must be within int range because fgets only accepts 10 chars
+        if (input_val < INT_MIN || input_val > INT_MAX)
+        {
+            printf("Value out of range for integer. Try again.\n");
+            continue;
+        }
 
-        if(value < min || (value > max)){
+        value = (int)input_val;
+        if (value < min || (value > max))
+        {
             printf("Not in bounds! Enter a value between %d and %d!\n", min, max);
             continue;
         }
 
         return value;
     }
-
 }
 
 int ui_get_params(Config *p_config)
@@ -99,7 +104,6 @@ p_config->random_seed          <- ui_get_int("Enter your seed (random number): "
 return 0
 */
 {
-
 }
 
 int ui_write_head(const Config *p_config, const char *stats_names, FILE *fp)
@@ -114,9 +118,7 @@ fputs(stats_names, fp)              //new line
 return 0
 */
 {
-
 }
-
 
 int ui_write_stats(const Stats *p_stats, FILE *fp)
 /* PSEUDOCODE
@@ -128,9 +130,7 @@ with fprintf(), write the current timestep and the 5 stats into txt file, sepera
 return 0
 */
 {
-
 }
-
 
 void ui_print_stats(const Stats *p_stats)
 /* PSEUDOCODE
@@ -139,7 +139,7 @@ IF p_stats = NULL THEN
     return -1
 END IF
 OUTPUT current_timestep                 // global variable
-OUTPUT p_stats->occupancy_rate   
+OUTPUT p_stats->occupancy_rate
 OUTPUT p_stats->cars_waiting
 OUTPUT p_stats->first_car_wait_time
 OUTPUT p_stats->avg_wait_time
@@ -148,14 +148,11 @@ return 0
 
 */
 {
-
 }
-
 
 void ui_print_final_stats(int avg_parking_duration, int avg_waiting_duration, int avg_stress_score)
 /* PSEUDOCODE
 OUTPUT message to user icluding integers
 */
 {
-
 }
