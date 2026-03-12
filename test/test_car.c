1d8cb9c6ea08798bb941cc7cb2dba8b41049613a
andrew <andrewcvolle@gmail.com>
2026-03-12 11:00:40 +0100

    upgrade error handling for test_car.c

--- test/test_car.c
+++ test/test_car.c
@@ -6,37 +6,40 @@ void test_init_car(){
     
     Car *p_car = init_car(1, 1, 1);
 
-    if(p_car == NULL){
-        return;
+    if(p_car != NULL){
+        // asserts that assigning the ID number works
+        assert(p_car->id_number == 1);
+
+        //asserts that the time in queue is zero-initialized
+        assert(p_car->time_in_queue == 0);
+
+        
+    } else{
+        printf("init car test error: null pointer\n");
     }
 
-    //asserts that init_car returns a pointer to something the size of a Car (presumably a Car struct)
-    assert(sizeof(Car) == (sizeof *p_car));
-
-    // asserts that assigning the ID number works
-    assert(p_car->id_number == 1);
-
-    //asserts that the time in queue is zero-initialized
-    assert(p_car->time_in_queue == 0);
-
     free(p_car);
+
+
+
 
 }
 
 void test_delete_car(){
     Car *p_car = init_car(2, 3, 5);
 
-    if(p_car == NULL){
-        return;
+    if(p_car != NULL){
+        //asserts that delete_car successfully frees the memory of a Car pointer
+        assert(delete_car(p_car) == 0);
+
+        p_car = NULL;
+
+        //asserts that passing a null pointer returns a managable error message
+        assert(delete_car(p_car) == -1);
+    } else {
+        printf("delete car tes error: null pointer");
     }
-    
-    //asserts that delete_car successfully frees the memory of a Car pointer
-    assert(delete_car(p_car) == 0);
 
-    p_car = NULL;
-
-    //asserts that passing a null pointer returns a managable error message
-    assert(delete_car(p_car) == -1);
 
 }
 
