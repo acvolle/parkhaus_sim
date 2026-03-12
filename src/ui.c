5380b27f44a3bf50455a66c2e4d9e9211dd85484
andrew <andrewcvolle@gmail.com>
2026-03-12 18:17:46 +0100

    add usage of ui_print_border

--- src/ui.c
+++ src/ui.c
@@ -33,10 +33,12 @@ OUTPUT welcome message
 */
 {
     printf("Welcome to the Parkhaus P4 Rauenegg simultion tool!\n");
+    ui_print_border();
     printf("- You will be asked to input simulation parameters\n");
     printf("- The simulation will run, and statistics for each timeste pwill be printed\n");
     printf("- Final statistics will be printed at the end\n");
-    printf("- The simulation will end and all stats will be readable in a stats.txt file\n\n");
+    printf("- The simulation will end and all stats will be readable in a stats.txt file\n");
+    ui_print_border();
 }
 
 /**
